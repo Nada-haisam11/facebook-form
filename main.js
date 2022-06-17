ae542894c5bfcfcc040a01ac0656600a13a498ea
Nada-haisam11 <nadaaboelea800@gmail.com>
2022-06-18 02:16:28 +0300

    sceand app

---
+++ main.js
@@ -0,0 +1,12 @@
+function nada(){
+
+    var email = document.getElementById("email"). value;
+
+    var password = document.getElementById("pasword").value;
+
+
+    if(email.indexof("@")==-1){
+         
+    }
+
+}
