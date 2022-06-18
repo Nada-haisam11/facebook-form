96b426e4e7961f446ef9e23d3de09bb8b9738bdc
Nada Haisam <107724068+Nada-haisam11@users.noreply.github.com>
2022-06-18 23:46:04 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,18 @@
-function nada(){
-
-    var email = document.getElementById("email"). value;
-
-    var password = document.getElementById("pasword").value;
-
-
-    if(email.indexof("@")==-1){
-         
+function fromValidate(params) {
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+    var error = document.getElementById("error");
+    var text = "";
+    if(email.indexOf("@") == -1) {
+        text = "please enter a valid email !";
+        error.innerHTML = text;
+        return false;
+    } else if (password.length < 5){
+        text = "please enter a valid password !";
+        error.innerHTML = text;
+        return false;
+    } else {
+        alert("Done");
+        return true ;
     }
-
 }
